6267e88fe1529373d4cb2d4f4888c034c35ae0dd
Jay Patel <pateljay.ram00@gmail.com>
2020-07-31 23:59:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Management.Sql.Fluent.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureSQLDBCreator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Initializing Connection...");
+            var sqlManager = new AzureSqlManager("tcbFinalResGrp", "tcbcoresqlserver");
+            Console.WriteLine("Connection setup successfully.");
+            Console.WriteLine("Enter Database name...");
+            var databaseName = Console.ReadLine();
+            Console.WriteLine($"Create Database {databaseName} in Canada Central region...");
+            try
+            {
+                var startTime = DateTime.UtcNow;
+                var status = await sqlManager.CreateDatabaseAsync(databaseName, "tcbcoreelasticpool", "Canada Central", CreateMode.Default, DatabaseEdition.Standard);
+                var totalSecondElapsed = DateTime.UtcNow.Subtract(startTime).TotalSeconds;
+
+                if (status)
+                    Console.WriteLine("Database created successfully.");
+                else
+                    Console.WriteLine($"Failed to create database.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to create database.");
+                Console.WriteLine($"Message: {e.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TokenServices/AzureTokenService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+
+namespace AzureSQLDBCreator.TokenServices
+{
+    public static class AzureTokenService
+    {
+        public static AzureCredentials GetAzureCredentials()
+        {
+            var subscriptionId = "3833f073-c643-4862-935b-d09d8baaa495";
+            var appId = "37a48cda-b1cb-4d60-8eaa-f58f6bc6985b";
+            var appSecret = "xOnTbK5Kzae1o-23-.2-MUQp.9okFmm7ru";
+            var tenantId = "6c031f94-c402-433a-92d2-2d3ce8516da3";
+            var environment = AzureEnvironment.AzureGlobalCloud;
+
+            var credentials = new AzureCredentialsFactory().FromServicePrincipal(appId, appSecret, tenantId, environment);
+
+            return credentials.WithDefaultSubscription(subscriptionId);
+        }
+    }
+
+}
